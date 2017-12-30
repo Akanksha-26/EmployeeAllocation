0ab3e3ef73223ede968e4ca284d17fdab7dd17fb
DhruvD <dhruv.datt@tv2consulting.com>
2017-12-30 14:54:26 +0530

    added sample data added project relation wih employee updated fetch api

--- client/src/Actions/projectActions.js
+++ client/src/Actions/projectActions.js
@@ -2,7 +2,12 @@ export const FETCH_PROJECTS = 'FETCH_PROJECTS';
 
 export const fetchProjects = () => {
     return dispatch => {
-        fetch('http://localhost:3000/api/projects', {'method':'GET'})
+        var filter = {
+            include:["employees"]
+        }
+        var url = 'http://localhost:3000/api/projects?filter=' + JSON.stringify(filter);
+
+        fetch(url, {'method':'GET'})
         .then((resp) => resp.json())
         .then(data => {
             dispatch({
