fd532b0418598d6d3d3f812fe763282a161702ae
DhruvD <dhruv.datt@tv2consulting.com>
2017-12-30 12:01:56 +0530

    restore home page

--- client/src/Containers/HomePage/index.js
+++ client/src/Containers/HomePage/index.js
@@ -2,12 +2,23 @@ import React from 'react'
 import { push } from 'react-router-redux'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import logo from './logo.svg';
+import './index.css';
 
 const Home = props => (
   <div>
     <h1>Home</h1>
     <p>Welcome home!</p>
-    <button onClick={() => props.changePage()}>Go to about page via redux</button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+      <button onClick={() => props.changePage()}>Go to about page via redux</button>
+    </div>
   </div>
 )
 
