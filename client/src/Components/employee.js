4cd026805f9d8979fd0d57d748e8ee050aee5548
Akanksha <akanksha.sharma@monjin.com>
2017-12-30 15:59:24 +0530

    template integration with fetch project api

--- client/src/Components/employee.js
+++ client/src/Components/employee.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const Employee = ({employee}) => {
+const Employee = ({employee, currentProjectName}) => {
     return (
         <div className="project__employee">
             <div className="project__employee__title"></div>
-            <span className="popover above">Hey bro, I am {employee.name}!</span>
+            <span className="popover above">
+                <h3 className="popover__name"> {employee.name} </h3>
+                <h5 className="popover__name">{employee.role}</h5>
+                <h6 className="popover__title">Team</h6>
+                <h4></h4>
+                <h6 className="popover__title">Current Project</h6>
+                <h4>{currentProjectName}</h4>
+            </span>
         </div>
     )
 }
--- client/src/Components/project.js
+++ client/src/Components/project.js
@@ -3,9 +3,22 @@ import Employee from './employee'
 export default class Project extends Component{
     render = () => {
         let { project } = this.props;
-        let employees = project.employees.map(employee => {
-            return <Employee key={employee.id} employee={employee}/>
+        let dummyEmployee = {
+            name:'No allocation',
+            role:'N/A',
+            id:-1,
+            projectId:project.id
+        }
+        let employees = project.employees.map((employee,index) => {
+            return <Employee key={employee.id} employee={employee} currentProjectName = {project.name}/>
         })
+
+        if(project.employees.length < 4){
+            for(let i=0; i < 4 - project.employees.length;i++ ){
+                employees.push(<Employee key={i} employee={dummyEmployee} currentProjectName = 'N/A' />);
+            }
+        }
+
         return(
             <div className="project">
                 <div className="project__employee-grid">
